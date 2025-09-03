04b1efd264df26ccd1eb34a01d313b92fad45df8
Deepika <136628175+Deepikbabu@users.noreply.github.com>
2025-09-03 17:31:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,39 @@ import simpleGit from "simple-git";
 import random from "random";
 
 const path = "./data.json";
+const git = simpleGit();
 
-const markCommit = (x, y) => {
-  const date = moment()
+// Function to generate a random date within the last year
+const generateRandomDate = () => {
+  const weeks = random.int(0, 54); // random week
+  const days = random.int(0, 6);   // random day
+  return moment()
     .subtract(1, "y")
     .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
+    .add(weeks, "w")
+    .add(days, "d")
     .format();
-
-  const data = {
-    date: date,
-  };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
 };
 
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+// Function to create a single commit
+const makeCommit = async () => {
+  const date = generateRandomDate();
+  const data = { date };
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
+  await jsonfile.writeFile(path, data);       // Update JSON
+  await git.add([path]);                      // Stage file
+  await git.commit(`Commit for ${date}`, { '--date': date }); // Commit
+  console.log(`Committed: ${date}`);
 };
 
+// Main function to generate multiple commits
+const makeCommits = async (n) => {
+  for (let i = 0; i < n; i++) {
+    await makeCommit();
+  }
+  await git.push(); // Push all commits at once
+  console.log("All commits pushed successfully!");
+};
+
+// Generate 100 commits
 makeCommits(100);
